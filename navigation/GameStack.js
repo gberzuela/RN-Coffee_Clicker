637f04f19de7abdf5a46343242c5a4ee1872367d
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-07 22:52:04 -0800

    adds logout button to GameStack

--- navigation/GameStack.js
+++ navigation/GameStack.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import GameScreen from '../screens/GameScreen';
+import { AuthContext } from '../navigation/AuthProvider';
+
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
 export default function GameStack() {
+	const { logout } = useContext(AuthContext);
+
 	return (
 		<Stack.Navigator initialRouteName={'Game'}>
 			<Stack.Screen
 				name="Game"
 				component={GameScreen}
-				options={{ header: () => null }}
+				options={({ navigation }) => ({
+					title: '',
+					headerStyle: {
+						backgroundColor: '#f8fafd',
+						shadowColor: '#f8fafd',
+					},
+					headerLeft: () => (
+						<SimpleLineIcons
+							style={{ marginLeft: 10 }}
+							name="logout"
+							size={26}
+							backgroundColor="#f8fafd"
+							onPress={() => logout()}
+						/>
+					),
+				})}
 			/>
 		</Stack.Navigator>
 	);
