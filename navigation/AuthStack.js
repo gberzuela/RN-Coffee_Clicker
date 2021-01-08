73b1fc9d88a327c8cc8558d4e15ee9b43e5e1861
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-07 22:53:32 -0800

    cleans imports for AuthStack

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 
 import { FontAwesome } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
